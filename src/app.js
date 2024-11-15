17fdf7cb54430df63074a13b602dbc06b3a1053a
Samriddhiy <samriddhi4005@gmail.com>
2024-11-16 01:23:17 +0530

    working on til

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express"
+import cors from "cors"
+import cookie-paser from "cookie-paraser"
+
+const app = express()
+
+app.use(cors)
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -9,3 +9,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongodb is not able to connect!!!", err);
+})

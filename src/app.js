e94468573cf6827dfb2d175a4afafe792fd2d346
Samriddhiy <samriddhi4005@gmail.com>
2024-11-17 02:03:18 +0530

    register

--- src/app.js
+++ src/app.js
@@ -14,5 +14,17 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import userRouter from "./routes.user.routes.js"
+
+app.use("/api/v1/users" , userRouter)
+
+
+
+
+
+
+
+
+
 
 export {app}
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,64 @@
+import { asyncHandler } from "../utils/AsyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse";
+import fs from "fs";
+
+
+
+
+
+
+const registerUser = async (req, res) => {
+    const imageFile = req.files["image"] ? req.files["image"][0] : null;
+    const { fullname, username, email, password } = req.body;
+    console.log("name", fullname);
+    console.log("username", username); 
+    console.log("email", email);
+    console.log(password);
+    let uploadResponse;
+    if (imageFile) {
+        try {
+            uploadResponse = await uploadOnCloudinary(imageFile.path);
+            fs.unlinkSync(imageFile.path);
+        } catch (error) {
+            return res.status(500).json({ message: "Image upload to Cloudinary failed." });
+        }
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+     console.log(existedUser)
+       if (existedUser) {
+        return res.status(400).json({
+            status: 400,
+            message: "User already exists with this email or username",
+          });
+        }
+        console.log("there")
+        const user = await User.create({
+            fullname,
+            image: uploadResponse?.url || "",
+            email,
+            password,
+            username: username.toLowerCase(),
+        });
+        const createdUser = await User.findById(user._id).select(
+            "-password -refreshToken"
+        );
+        console.log(createdUser)
+        if (!createdUser) {
+            return res
+            .status(500)
+            .json({ message: "User registration failed. Please try again." });
+        }
+        return res
+        .status(201)
+        .json(new ApiResponse(200, createdUser, "User registered successfully"));
+};
+
+
+export {
+    registerUser
+};
